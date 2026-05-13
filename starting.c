c6dc5106655a746b05a24dc4df284e09d890fccd
aperf3ct <158367984+aperf3ct@users.noreply.github.com>
2026-05-14 10:18:55 +1200

    Update starting.c
    half finished

--- starting.c
+++ starting.c
@@ -12,36 +12,45 @@
 #include <stdbool.h>
 #include "camera_x11.h"
 
-
 //using namespace std;
 
+bool isRubyPresent(int current){
+	if (current != 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
 int main() {
-	
+
 	 if (start_camera() != 0) {
         printf("Failed to start camera\n");
         return 1;
     }
-   
+
   // make 1000 runs  
   for (int countrun = 0; countrun < 10000; countrun++) {
 	take_picture();
-	int totRed = 0;
-	int totInt = 0;
-	double redness = 0.0;
+	display_picture();
 
-    // for all pixels in latest image
+	int currentRedPixelCount = 0;
+	
+    
+	// for all pixels in latest image
     for (int row = 0 ; row < 480 ; row++) {	
 		for (int col = 0; col < 640; col++) {
 			uchar r, g ,b ;
 			get_pixel(row, col, &r, &g ,&b);
-			totRed = totRed + r;
-			totInt = totInt + (r+g+b)/3;
-			redness = (double)totRed/(3.0*(double)totInt);
+
+			if (r > 150 && r > g + 40 && r > b + 40) { 
+                currentRedPixelCount++; 
+
+			}
 		}
 	}
-
-	//change later, for now is just a placeholder for when the ruby has been moved
-	bool rubyPresent = false;
+	bool rubyPresent = isRubyPresent(currentRedPixelCount);
 
 	//if ruby is present
 	if(rubyPresent){
@@ -56,25 +65,23 @@ int main() {
 	}else{
 		//ruby is not present
 		printf("Ruby is not present\n");
+
 		for (int row = 0; row < 480; row++) {
                 for (int col = 0; col < 640; col++) {
                     set_pixel(row, col, 255, 0, 0);
-					return 0;
                 }
             }
+		return 0;
 	}
-
-	//prints data
-	printf(" countrun: %d\n",countrun);
-	printf(" Total red:%d\n",totRed);
-	printf(" Total intensity: %d\n",totInt);
-	printf("  redness : %f\n", redness);
-
-	//prints the screen
-	display_picture();
-	sleep1(1000); // slow down a bit to make display easier
 	
-  }  
-  //close_screen_stream();
-  return 0;
+	//close_screen_stream();
+	//return 0;	
+
 }
+	
+
+  }  
+
+
+
+
