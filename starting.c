811d63fe73f295463902f99bcf37097c7af2c231
aperf3ct <158367984+aperf3ct@users.noreply.github.com>
2026-05-14 10:45:50 +1200

    visual feedback function

--- starting.c
+++ starting.c
@@ -16,12 +16,36 @@
 //using namespace std;
 
 bool isRubyPresent(int current){
-	if (current != 0){
+	if (current > 100){
 		return true;
 	}
 	else{
 		return false;
 	}
+}
+
+void applyVisualFeedback(bool found){
+	if (found){
+		//makes square green
+		for (int row = 0; row < 50; row++) {
+                for (int col = 0; col < 50; col++) {
+                    set_pixel(row, col, 0, 255, 0);
+                }
+            }
+	} else{
+		//gets time and prints it
+		time_t currentTime;
+    	time(&currentTime);
+		printf("Current time: %s", ctime(&currentTime)); 
+
+		//makes screen red
+		for (int row = 0; row < 480; row++) {
+                for (int col = 0; col < 640; col++) {
+                    set_pixel(row, col, 255, 0, 0);
+                }
+            }
+	}
+
 }
 
 int main() {
@@ -51,45 +75,17 @@ int main() {
 		}
 	}
 	bool rubyPresent = isRubyPresent(currentRedPixelCount);
-
-	//if ruby is present
-	if(rubyPresent){
-		//printf("Ruby is present\n");
-
-		//makes green square
-		for (int row = 0; row < 50; row++) {
-                for (int col = 0; col < 50; col++) {
-                    set_pixel(row, col, 0, 255, 0);
-                }
-            }
-		
-
-	}else{
-		//ruby is not present
-		//printf("Ruby is not present\n");
-
-		//gets time and prints it
-		time_t currentTime;
-    	time(&currentTime);
-		printf("Current time: %s", ctime(&currentTime)); 
-
-		//makes screen red
-		for (int row = 0; row < 480; row++) {
-                for (int col = 0; col < 640; col++) {
-                    set_pixel(row, col, 255, 0, 0);
-                }
-            }
-	}
+	
+	applyVisualFeedback(rubyPresent);
 	
 	display_picture();
 
 	//close_screen_stream();
 	//return 0;	
 
-}
+	}
 	
-
-  }  
+}  
 
 
 
